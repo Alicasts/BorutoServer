2a628251072a9e15845742f9d04a3293c901bbbe
Ali <ali.cast.santos@gmail.com>
2024-01-24 15:56:37 -0300

    procfile and gradle build modif

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,9 +3,8 @@ package com.example
 import com.example.plugins.*
 import io.ktor.server.application.*
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
-}
 
 fun Application.module() {
     configureKoin()
